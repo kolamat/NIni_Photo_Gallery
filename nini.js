af271a63a3eed46bc741a52c03b9b0ab540f36a3
kolamat <matanmidupe004@gmail.com>
2024-08-12 03:10:50 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   nini.css
    	modified:   nini.html
    	modified:   nini.js

--- nini.js
+++ nini.js
@@ -17,7 +17,7 @@ document.getElementById("header").appendChild(video);
 //Section_One
 const nini = document.createElement("h1");
 nini.textContent =
-  "Welcome to your best and second year on earth wishing you Long life and more prosperity in life and a Happy Birthday ";
+  "Welcome to your best and second year on earth wishing you Long life and prosperity in life and a Happy Birthday ";
 document.querySelector("body").appendChild(nini);
 
 function startCountdown(startNumber) {
